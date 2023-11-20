e6400d4edfba4dbb2c39dd4f2d029e38061333a3
Flames <isaacomenuche@gmail.com>
2023-11-20 12:16:38 +0100

    homepage desktop view

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -428,6 +428,19 @@ function Home() {
             </ul>
           </div>
         </div>
+        <div className=" bg-[#F5F5F5]">
+          <div className="flex items-center flex-col w-11/12 m-auto py-8">
+            <h2 className="text-3xl text-black font-bold">
+              Your Farm Deserves a Wider Reach
+            </h2>
+            <p className="text-black text-2xl mt-4">
+              Join the farmers on farm connect
+            </p>
+            <button className="bg-[#008033] rounded text-white py-2 px-4 mt-4">
+              Become a Farmer
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
